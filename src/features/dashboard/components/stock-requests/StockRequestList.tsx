0a8e3692ff7833318e93ddf337e03f6ddaabf6e5
j-tee <juliustetteh@gmail.com>
2025-10-14 21:51:43 +0000

    fix: Remove React.memo wrapper - last attempt to fix focus loss
    Removing React.memo in case it's causing comparison issues that
    trigger unnecessary re-renders. This is the final attempt before
    replacing the search with a different UI pattern.

--- src/features/dashboard/components/stock-requests/StockRequestList.tsx
+++ src/features/dashboard/components/stock-requests/StockRequestList.tsx
@@ -1,4 +1,4 @@
-import { type ChangeEvent, memo } from 'react'
+import { type ChangeEvent } from 'react'
 import Alert from 'react-bootstrap/Alert'
 import Badge from 'react-bootstrap/Badge'
 import Button from 'react-bootstrap/Button'
@@ -288,4 +288,4 @@ const StockRequestList = ({
   )
 }
 
-export default memo(StockRequestList)
+export default StockRequestList
